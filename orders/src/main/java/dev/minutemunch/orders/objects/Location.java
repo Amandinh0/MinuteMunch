abfc652143ca84434a909d3c2219a8365ddd9a96
Austin Fairbanks <austinjf2005@gmail.com>
2023-11-10 23:40:39 -0500

    Add order object, location object, and link spring boot

---
+++ orders/src/main/java/dev/minutemunch/orders/objects/Location.java
@@ -0,0 +1,22 @@
+package dev.minutemunch.orders.objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class Location {
+    private ObjectId id;
+    private String residenceHall;
+    private String residenceArea;
+
+    public Location(String residenceHall, String residenceArea) {
+        this.residenceArea = residenceArea;
+        this.residenceHall = residenceHall;
+    }
+}

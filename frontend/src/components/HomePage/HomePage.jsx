afc775833d005d5552e75349bf3e862f140e39fb
SecureDAO <93850661+SecureDAO@users.noreply.github.com>
2023-11-11 17:09:31 -0500

    Front Page Design

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,34 +1,47 @@
 import React from "react";
-import {useNavigate} from "react-router-dom"; 
-
+import { useNavigate } from "react-router-dom";
 
 const SplitLayout = () => {
-  const navigate = useNavigate(); 
+	const navigate = useNavigate();
 
-  function routeConsumer(){
-    navigate('/consumerHome');
-  }
+	function routeConsumer() {
+		navigate("/consumerHome");
+	}
 
-  function routeDriver(){
-    navigate('/driverHome')
-  }
+	function routeDriver() {
+		navigate("/driverHome");
+	}
 
-    return (
-      <div className="split-layout">
-        <div className="left-half">
-          {/* Button for the left half */}
-          <button onClick={routeConsumer}>Left</button>
-        </div>
-        <div className="right-half">
-          {/* Button for the right half */}
-          <button onClick={routeDriver}>Right</button>
-        </div>
-      </div>
-    );
-  };
+	return (
+		<section>
+			<div
+				className="left-half"
+				style={{
+					backgroundImage: `url("https://optimoroute.com/wp-content/uploads/2021/05/Delivery-Driver-Safety-Tips-HERO.jpg")`,
+					backgroundSize: "cover",
+					backgroundColor: "tan",
+					backgroundBlendMode: "screen",
+				}}
+			>
+				<button type="button" id="left-button" onClick={routeDriver}>
+					Become a Driver
+				</button>
+			</div>
+			<div
+				className="right-half"
+				style={{
+					backgroundImage: `url("https://people.com/thmb/fybOj-ImWw2cqJg9tFn3aMV1vKE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/50-foods-kevin-hart-092022-856e0ffe414a406e8f2060345303ac5c.jpg")`,
+					backgroundSize: "cover",
+					backgroundColor: "maroon",
+					backgroundBlendMode: "screen",
+				}}
+			>
+				<button type="button" id="right-button" onClick={routeDriver}>
+					Want to Munch
+				</button>
+			</div>
+		</section>
+	);
+};
 
-  
-  
-  
-  export default SplitLayout;
-
+export default SplitLayout;

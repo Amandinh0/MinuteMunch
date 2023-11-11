c922faff6dd0b963857fb095fff6e51a54b3ddfd
SecureDAO <93850661+SecureDAO@users.noreply.github.com>
2023-11-11 12:24:04 -0500

    asd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import './styles/styles.css';
-import SplitLayout from './components/SplitLayout';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ConsumerLayout from './pages/ConsumerLayout';
-import DriverLayout from './components/DriverLogin/DriverLogin';
-
+import "./components/HomePage/style.module.css";
+import "./styles.css";
+import SplitLayout from "./components/HomePage/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ConsumerLayout from "./components/ConsumerLogin/ConsumerLogin";
+import DriverLayout from "./components/DriverLogin/DriverLogin";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/consumerHome" element={<ConsumerLayout />} />
-        <Route path="/driverHome" element={<DriverLayout />} />
-        <Route path="/" element={<SplitLayout />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/consumerHome" element={<ConsumerLayout />} />
+				<Route path="/driverHome" element={<DriverLayout />} />
+				<Route path="/" element={<SplitLayout />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;

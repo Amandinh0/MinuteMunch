62b95c344d0b2686ec05d45bdeee3f5a85e59f73
SecureDAO <93850661+SecureDAO@users.noreply.github.com>
2023-11-12 08:51:47 -0500

    Try again

--- frontend/src/components/OrderDetailPreview/OrderDetailPreview.jsx
+++ frontend/src/components/OrderDetailPreview/OrderDetailPreview.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import "./OrderDetailPreview.css";
-import globalVariables from "../OrderListItem/globalVariables";
+import globalVariables from '../OrderListItem/globalVariables';
 
 function OrderDetailPreview({ order, onClickItem }) {
 	console.log("from Detail", order);
@@ -42,73 +42,38 @@ function OrderDetailPreview({ order, onClickItem }) {
 				<div class="container">
 					{/* <h2>Entree</h2> */}
 
-					<table>
-						<tbody>
-							<tr>
-								<th>Entree1:</th>
-								<td>
-									{order.foodList[0] == null ? "None" : order.foodList[0]}
-								</td>
-							</tr>
-							<tr>
-								<th>Entree2:</th>
-								<td>
-									{order.foodList[1] == null ? "None" : order.foodList[1]}
-								</td>
-							</tr>
-							<tr>
-								<th>Entree2:</th>
-								<td>
-									{order.foodList[2] == null ? "None" : order.foodList[2]}
-								</td>
-							</tr>
-							<tr>
-								<th>Drink:</th>
-								<td>{order.drink == null ? "None" : order.drink}</td>
-							</tr>
-							<tr>
-								<th>Delivery Location:</th>
-								<td>{order.location.residenceHall}</td>
-							</tr>
-							<tr>
-								<th>Pickup Location:</th>
-								<td>{order.diningHall}</td>
-							</tr>
-						</tbody>
-					</table>
+            <table>
+            <tbody>
+                <tr>
+                <th>Entree1:</th>
+                <td>{order.foodList[0] == null ? "None" : order.foodList[0]}</td>
+                </tr>
+                <tr>
+                <th>Entree2:</th>
+                <td>{order.foodList[1] == null ? "None" : order.foodList[1]}</td>
+                </tr>
+                <tr>
+                <th>Entree2:</th>
+                <td>{order.foodList[2] == null ? "None" : order.foodList[2]}</td>
+                </tr>
+                <tr>
+                <th>Drink:</th>
+                <td>{order.drink == null ? "None" : order.drink}</td>
+                </tr>
+                <tr>
+                <th>Delivery Location:</th>
+                <td>{order.location.residenceHall}</td>
+                </tr>
+                <tr>
+                <th>Pickup Location:</th>
+                <td>{order.diningHall}</td>
+                </tr>
+            </tbody>
+            </table>
 
-					<table>
-						<tbody>
-							<tr>
-								<th>Entree:</th>
-								<td>MEAL</td>
-							</tr>
-							<tr>
-								<th>Side1:</th>
-								<td>MILK</td>
-							</tr>
-							<tr>
-								<th>Side2:</th>
-								<td>MILK</td>
-							</tr>
-							<tr>
-								<th>Side3:</th>
-								<td>MILK</td>
-							</tr>
-							<tr>
-								<th>Side4:</th>
-								<td>MILK</td>
-							</tr>
-							<tr>
-								<th>Delivery Location:</th>
-								<td>MILK</td>
-							</tr>
-							<tr>
-								<th>Pickup Location:</th>
-								<td>MILK</td>
-							</tr>
-						</tbody>
-					</table>
+            <div>
+              <button onClick={RouteHome} className="accept-button">Accept Order</button><button onClick={RouteBack} className="reject-button">Reject Order</button>
+            </div>
 
 					<div class="total">
 						<h3 class="money">${order.payout}</h3>

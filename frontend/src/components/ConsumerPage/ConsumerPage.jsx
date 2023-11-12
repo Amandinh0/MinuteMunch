8ad817d419e090e1b8444872dacc397dd520b4f3
SecureDAO <93850661+SecureDAO@users.noreply.github.com>
2023-11-12 05:41:31 -0500

    merge that

--- frontend/src/components/ConsumerPage/ConsumerPage.jsx
+++ frontend/src/components/ConsumerPage/ConsumerPage.jsx
@@ -1,58 +1,73 @@
-import { useState } from 'react';
-import foodMenus from '../data/menus.json'; 
-import DiningPopUp from './DiningPopUp';
+import { useState } from "react";
+import foodMenus from "../data/menus.json";
+import DiningPopUp from "./DiningPopUp";
 
+function ConsumerPage() {
+	const [buttonPopup, setbuttonPopup] = useState(false);
+	const [hallName, sethallName] = useState(null);
+	const [hallString, setHallString] = useState("");
 
+	const wooClicked = () => {
+		// <DiningPopUp hallName={foodMenus.Worcester} drinks={foodMenus.Drinks}/>
+		sethallName(foodMenus.Worcester);
+		setbuttonPopup(true);
+		setHallString("Worcester");
+	};
+	const frankClicked = () => {
+		sethallName(foodMenus.Franklin);
+		setbuttonPopup(true);
+		setHallString("Franklin");
+	};
 
-function ConsumerPage(){
+	const hampClicked = () => {
+		sethallName(foodMenus.Hampshire);
+		setbuttonPopup(true);
+		setHallString("Hampshire");
+	};
 
-    const [buttonPopup, setbuttonPopup] = useState(false); 
-    const[hallName, sethallName] = useState(null);
-    const [hallString, setHallString] = useState(''); 
+	const berkClicked = () => {
+		sethallName(foodMenus.Berkshire);
+		setbuttonPopup(true);
+		setHallString("Berkshire");
+	};
 
-
-    const wooClicked =()=>{
-        // <DiningPopUp hallName={foodMenus.Worcester} drinks={foodMenus.Drinks}/>
-        sethallName(foodMenus.Worcester);
-        setbuttonPopup(true);
-        setHallString("Worcester"); 
-
-    }
-    const frankClicked =()=>{
-        sethallName(foodMenus.Franklin);
-        setbuttonPopup(true); 
-        setHallString("Franklin");
-    
-    }
-    
-    const hampClicked =()=>{
-        sethallName(foodMenus.Hampshire);
-        setbuttonPopup(true);
-        setHallString("Hampshire");
-        
-    }
-    
-    const berkClicked =()=>{
-        sethallName(foodMenus.Berkshire);
-        setbuttonPopup(true);
-        setHallString("Berkshire");
-        
-    }
-    
-
-
-    return(
-        <>
-            
-            <button className='button-dining woo' onClick={wooClicked}> Worcester </button>
-            <button className='button-dining frank' onClick={frankClicked}> Franklin </button>
-            <button className='button-dining hamp' onClick={hampClicked}> Hampshire </button>
-            <button className='button-dining berk' onClick={berkClicked}> Berkshire </button>
-            <DiningPopUp trigger={buttonPopup} setTrigger={setbuttonPopup} hall={hallName} drink={foodMenus.Drinks} 
-            hallName={hallString}/>
-        </>
-    )
+	return (
+		<>
+			<div className="button-grid">
+				<div className="woo">
+					<button className="button-diningwoo" onClick={wooClicked}>
+						{" "}
+						Worcester{" "}
+					</button>
+				</div>
+				<div className="frank">
+					<button className="button-diningfrank" onClick={frankClicked}>
+						{" "}
+						Franklin{" "}
+					</button>
+				</div>
+				<div className="hamp">
+					<button className="button-dininghamp" onClick={hampClicked}>
+						{" "}
+						Hampshire{" "}
+					</button>
+				</div>
+				<div className="berk">
+					<button className="button-diningberk" onClick={berkClicked}>
+						{" "}
+						Berkshire{" "}
+					</button>
+				</div>
+				<DiningPopUp
+					trigger={buttonPopup}
+					setTrigger={setbuttonPopup}
+					hall={hallName}
+					drink={foodMenus.Drinks}
+					hallName={hallString}
+				/>
+			</div>
+		</>
+	);
 }
 
-
-export default ConsumerPage
+export default ConsumerPage;

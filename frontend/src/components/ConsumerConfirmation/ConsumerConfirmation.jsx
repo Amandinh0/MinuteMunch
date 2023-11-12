328ee71c442c7e4b4d92150853d1aea838c1716a
Amandinh0 <amanchhetricr7@gmail.com>
2023-11-11 20:50:27 -0500

    menu items

--- frontend/src/components/ConsumerConfirmation/ConsumerConfirmation.jsx
+++ frontend/src/components/ConsumerConfirmation/ConsumerConfirmation.jsx
@@ -1,7 +1,13 @@
 import { useLocation } from "react-router-dom";
+import OrdersAPI from "../../api/orders";
 
 
 function ConsumerConfirmation(props){
+
+    function postOrder(){
+        console.log(JSON.stringify(parsedBreakfast),JSON.stringify(parsedLunch ));
+    }
+
     const location = useLocation();
     const { breakfast, lunch } = location.state;
 
@@ -11,15 +17,40 @@ function ConsumerConfirmation(props){
 
 
     return(
-        <>
-            <h1>This is conusmer confirmation</h1>
-            {/* <h1>{parsedBreakfast}</h1>
-            <h1>{parsedLunch}</h1> */}
-            <p>Breakfast items: {JSON.stringify(parsedBreakfast)}</p>
-            <p>Lunch items: {JSON.stringify(parsedLunch)}</p>
-        </>
+            <div>
+              <h1>Confirmation Page</h1>
+        
+              {parsedBreakfast.length > 0 && (
+                <div>
+                  <h2>Breakfast items:</h2>
+                  <ul>
+                    {parsedBreakfast.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+        
+              {parsedLunch.length > 0 && (
+                <div>
+                  <h2>Lunch items:</h2>
+                  <ul>
+                    {parsedLunch.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+        
+              <button onClick={postOrder}>Order</button>
+            </div>
+
+            // <p>Breakfast items: {JSON.stringify(parsedBreakfast)}</p>
+            //<p>Lunch items: {JSON.stringify(parsedLunch)}</p>
     );
 }
 
 export default ConsumerConfirmation; 
+
+
 

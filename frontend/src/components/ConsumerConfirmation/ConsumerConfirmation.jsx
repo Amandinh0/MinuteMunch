e6fd82b8384252ba8b2a2354243c9e88f6ede05d
Austin Fairbanks <austinjf2005@gmail.com>
2023-11-12 10:40:11 -0500

    git

--- frontend/src/components/ConsumerConfirmation/ConsumerConfirmation.jsx
+++ frontend/src/components/ConsumerConfirmation/ConsumerConfirmation.jsx
@@ -2,96 +2,89 @@ import { useLocation, useNavigate } from "react-router-dom";
 import OrdersAPI from "../../api/orders";
 import { useState, useEffect } from "react";
 import Order from "../../order";
-import Housing from "../data/locations.json"; 
+import Housing from "../data/locations.json";
 
+function ConsumerConfirmation({ post }) {
+	// const [currentOrder, setOrder] = useState();
 
+	// useEffect(() => {
+	//   if (currentOrder) {
+	//     postOrders(currentOrder.state);
+	//   }
+	// },[currentOrder])
 
-function ConsumerConfirmation({post}){
+	const [phoneNumber, setphoneNumber] = useState("");
+	const [residenceHall, setresidencehall] = useState("");
+	const [email, setemail] = useState("");
+	const [name, setname] = useState("");
 
-  // const [currentOrder, setOrder] = useState();
+	const handleNumber = (event) => {
+		setphoneNumber(event.target.value);
+	};
+	const handleHall = (event) => {
+		setresidencehall(event.target.value);
+	};
+	const handleEmail = (event) => {
+		setemail(event.target.value);
+	};
+	const handleName = (event) => {
+		setname(event.target.value);
+	};
 
-  // useEffect(() => {
-  //   if (currentOrder) {
-  //     postOrders(currentOrder.state);
-  //   }
-  // },[currentOrder])
+	const navigate = useNavigate();
 
+	const location = useLocation();
+	const { food, drink, hall } = location.state;
 
+	const parsedFood = JSON.parse(food);
+	const parsedDrink = JSON.parse(drink);
+	const parsedHall = JSON.parse(hall);
 
-    const [phoneNumber, setphoneNumber] = useState(''); 
-    const [residenceHall, setresidencehall] = useState(''); 
-    const [email, setemail] = useState(''); 
-    const [name, setname] = useState('');
+	const payout = 0.33;
 
-    const handleNumber = (event) =>{
-        setphoneNumber(event.target.value); 
-    }
-    const handleHall = (event) =>{
-        setresidencehall(event.target.value); 
-    }
-    const handleEmail = (event) =>{
-        setemail(event.target.value); 
-    }
-    const handleName = (event) =>{
-        setname(event.target.value); 
-    }
+	const postOrder = (e) => {
+		e.preventDefault();
+		let residenceArea = null;
+		let cost = 0;
 
-    const navigate = useNavigate(); 
+		switch (residenceHall) {
+			case "Brown":
+			case "McNamara":
+			case "Cashin":
+				residenceArea = "Sylvan";
+				break;
 
-    
+			case "Baker":
+			case "Brett":
+			case "Brooks":
+			case "Butterfield":
+			case "Chadbourne":
+			case "Gorman":
+			case "Greenough":
+			case "Wheeler":
+				residenceArea = "Central";
+				break;
 
-    const location = useLocation();
-    const { food, drink, hall } = location.state;
+			case "Birch":
+			case "Elm":
+			case "Linden":
+			case "Maple":
+			case "OakHill":
+			case "Sycamore":
+				residenceArea = "Honors";
+				break;
 
-    const parsedFood = JSON.parse(food);
-    const parsedDrink = JSON.parse(drink); 
-    const parsedHall = hall;
-
-    const payout = 0.33
-
-    const postOrder = e => {
-        e.preventDefault();
-        let residenceArea = null; 
-        let cost = 0; 
-
-        switch(residenceHall){
-            case "Brown":
-            case "McNamara":
-            case "Cashin":
-                residenceArea = "Sylvan";
-                break; 
-            
-            case "Baker":
-            case "Brett":
-            case "Brooks":
-            case "Butterfield":
-            case "Chadbourne":
-            case "Gorman":
-            case "Greenough":
-            case "Wheeler":
-                residenceArea = "Central";
-                break; 
-            
-            case "Birch":
-            case "Elm":
-            case "Linden":
-            case "Maple":
-            case "OakHill":
-            case "Sycamore":
-                residenceArea = "Honors";
-                break; 
-            
-            case "Crabtree":
-            case "Dwight":
-            case "Hamlin":
-            case "Johnson":
-            case "Knowlton":
-            case "Leach":
-            case "Lewis":
-            case "MaryLyon":
-            case "Thatcher":
-                residenceArea = "Northeast";
-                break; 
+			case "Crabtree":
+			case "Dwight":
+			case "Hamlin":
+			case "Johnson":
+			case "Knowlton":
+			case "Leach":
+			case "Lewis":
+			case "MaryLyon":
+			case "Thatcher":
+				residenceArea = "Northeast";
+				break;
 
             case "Cance":
             case "Coolidge":
@@ -175,70 +168,70 @@ function ConsumerConfirmation({post}){
         //"Northeast", "Leach", ["asdlifj", "lfkdna"], ["dasilfnd"], 3 , "4:00", 6.77, "030-933-0320", "TEST@umass.edu", "lfadksjf", "WOOO"
         console.log(pushOrder.state);
 
-        post(pushOrder);
+		post(pushOrder);
 
-        navigate('/consumerHome/confirmation/checkout');
-    }
+		navigate("/consumerHome/confirmation/checkout");
+	};
 
-    return (
-      <div>
-        <h1>Confirmation Page</h1>
-        {parsedFood.length > 0 && (
-          <div>
-            <h3>From {parsedHall}</h3>
-            <h2>Food items:</h2>
-            <ul>
-              {parsedFood.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-        <div>
-          <h2>Drink</h2>
-          <ul>{parsedDrink}</ul>
-        </div>
+	return (
+		<div>
+			<h1>Confirmation Page</h1>
+			{parsedFood.length > 0 && (
+				<div>
+					<h3>From {parsedHall}</h3>
+					<h2>Food items:</h2>
+					<ul>
+						{parsedFood.map((item, index) => (
+							<li key={index}>{item}</li>
+						))}
+					</ul>
+				</div>
+			)}
+			<div>
+				<h2>Drink</h2>
+				<ul>{parsedDrink}</ul>
+			</div>
 
-        <label>
-          Name:
-          <input type="text" value={name} onChange={handleName} />
-        </label>
-        <br></br>
-        <label>
-          Phone Number:
-          <input type="text" value={phoneNumber} onChange={handleNumber} />
-        </label>
-        <br></br>
-        <label>
-          Umass Email:
-          <input type="text" value={email} onChange={handleEmail} />
-        </label>
-        <br></br>
-        { <select
-        id="residenceDropdown"
-        value={residenceHall}
-        onChange={handleHall}
-      >
-        <option value="">Select a residence</option>
-        {Object.keys(Housing).map((residenceName) => (
-          <option key={residenceName} value={residenceName}>
-            {residenceName}
-          </option>
-        ))}
-      </select>
-      }
-      <br></br>
-      <button onClick={postOrder}>Order</button>
-        {/* <label>
+			<label>
+				Name:
+				<input type="text" value={name} onChange={handleName} />
+			</label>
+			<br></br>
+			<label>
+				Phone Number:
+				<input type="text" value={phoneNumber} onChange={handleNumber} />
+			</label>
+			<br></br>
+			<label>
+				Umass Email:
+				<input type="text" value={email} onChange={handleEmail} />
+			</label>
+			<br></br>
+			{
+				<select
+					id="residenceDropdown"
+					value={residenceHall}
+					onChange={handleHall}
+				>
+					<option value="">Select a residence</option>
+					{Object.keys(Housing).map((residenceName) => (
+						<option key={residenceName} value={residenceName}>
+							{residenceName}
+						</option>
+					))}
+				</select>
+			}
+			<br></br>
+			<button onClick={postOrder}>Order</button>
+			{/* <label>
           Residence Hall:
           <input type="text" value={residenceHall} onChange={handleHall} />
         </label>
         <br></br>
         <br></br>
         <button onClick={postOrder}>Order</button> */}
-      </div>
-
-    );
+		</div>
+	);
 }
 
-export default ConsumerConfirmation; 
+export default ConsumerConfirmation;

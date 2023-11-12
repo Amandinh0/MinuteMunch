fbc8c60d5c82a40e04bf2d4599fed6ea0d593cea
Austin Fairbanks <austinjf2005@gmail.com>
2023-11-11 23:21:45 -0500

    add user database

---
+++ orders/src/main/java/dev/minutemunch/orders/users/UserController.java
@@ -0,0 +1,45 @@
+package dev.minutemunch.orders.objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getOrders() {
+        return new ResponseEntity<List<Order>>(service.findAllOrders(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{email}")
+    //path variable lets framework know we want to convert path varible to string id
+    public ResponseEntity<Optional<Order>> getSingleOrder(@PathVariable String email) {
+        return new ResponseEntity<Optional<Order>>(service.singleOrder(email), HttpStatus.OK);
+    }
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    //tells framework that whatever we get as the request body we want to create a map of key string and value string called payload
+    public ResponseEntity<Order> createOrder(@RequestBody Order newOrder) {
+        return new ResponseEntity<Order>(service.addOrder(newOrder), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{email}")
+    public String deleteElement(@PathVariable String email) {
+        Optional<Order> order = service.singleOrder(email);
+        service.deleteOrder(order.get().getId());
+        return "deleted";
+    }
+}
---
+++ orders/src/main/java/dev/minutemunch/orders/users/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.minutemunch.orders.objects;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends MongoRepository<Order, ObjectId> {
+    Optional<Order> findByEmail(String email);
+}

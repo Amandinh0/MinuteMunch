d8a2988c3615f8944604a64c65ad1bfd00df010e
SecureDAO <93850661+SecureDAO@users.noreply.github.com>
2023-11-11 19:16:43 -0500

    Start Unblurred

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -3,50 +3,41 @@ import { useNavigate } from "react-router-dom";
 
 const SplitLayout = () => {
 	const navigate = useNavigate();
-	const [isLeftHovered, setIsLeftHovered] = useState(false);
-	const [isRightHovered, setIsRightHovered] = useState(false);
-
-	function routeConsumer() {
-		navigate("/consumerHome");
-	}
+	const [hoveredSection, setHoveredSection] = useState(null);
 
 	function routeDriver() {
 		navigate("/driverHome");
 	}
 
-	const leftStyle = {
-		backgroundImage: `url("https://swartzlaw.com/wp-content/uploads/2022/05/Are-Food-Delivery-Drivers-More-Prone-to-Massachusetts-Distracted-Driving-Crashes-food-delivery-drivers.jpg")`,
+	const sectionStyle = (section) => ({
+		backgroundImage: `url("${
+			section === "left"
+				? "https://optimoroute.com/wp-content/uploads/2021/05/Delivery-Driver-Safety-Tips-HERO.jpg"
+				: "https://people.com/thmb/fybOj-ImWw2cqJg9tFn3aMV1vKE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/50-foods-kevin-hart-092022-856e0ffe414a406e8f2060345303ac5c.jpg"
+		}")`,
 		backgroundSize: "cover",
-		backgroundColor: "tan",
+		backgroundColor: section === "left" ? "tan" : "maroon",
 		backgroundBlendMode: "screen",
-		filter: isLeftHovered ? "none" : "blur(5px)",
-	};
-
-	const rightStyle = {
-		backgroundImage: `url("https://people.com/thmb/fybOj-ImWw2cqJg9tFn3aMV1vKE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/50-foods-kevin-hart-092022-856e0ffe414a406e8f2060345303ac5c.jpg")`,
-		backgroundSize: "cover",
-		backgroundColor: "maroon",
-		backgroundBlendMode: "screen",
-		filter: isRightHovered ? "none" : "blur(5px)",
-	};
+		filter: hoveredSection && hoveredSection !== section ? "blur(5px)" : "none",
+	});
 
 	return (
 		<section>
 			<div
 				className="left-half"
-				style={leftStyle}
-				onMouseEnter={() => setIsLeftHovered(true)}
-				onMouseLeave={() => setIsLeftHovered(false)}
+				style={sectionStyle("left")}
+				onMouseEnter={() => setHoveredSection("left")}
+				onMouseLeave={() => setHoveredSection(null)}
 			>
 				<button type="button" id="left-button" onClick={routeDriver}>
-					Supply the Munch
+					Become a Driver
 				</button>
 			</div>
 			<div
 				className="right-half"
-				style={rightStyle}
-				onMouseEnter={() => setIsRightHovered(true)}
-				onMouseLeave={() => setIsRightHovered(false)}
+				style={sectionStyle("right")}
+				onMouseEnter={() => setHoveredSection("right")}
+				onMouseLeave={() => setHoveredSection(null)}
 			>
 				<button type="button" id="right-button" onClick={routeDriver}>
 					Want to Munch
